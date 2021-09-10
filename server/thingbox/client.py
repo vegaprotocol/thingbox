476ee6b64c9be1e2bced565ca9770a563a709a76
Barney Mannerings <b.mannerings@gmail.com>
2021-09-10 11:01:12 +0100

    Initial updates

--- server/thingbox/client.py
+++ server/thingbox/client.py
@@ -24,13 +24,27 @@ def encrypt(plaintext, public_key_b58):
 	return b64encode(ciphertext).decode('utf-8')
 
 
-def add_item(server_base_url, auth_token, target_type, target_id, item_plaintext):
+def add_item(
+		server_base_url, 
+		auth_token, 
+		target_type, 
+		target_id, 
+		category, 
+		data_plaintext, 
+		template_id, 
+		batch_id=None):
 	public_key = get_public_key(server_base_url)
-	item_encrypted_b64 = encrypt(item_plaintext, public_key)
+	data_encrypted_b64 = encrypt(data_plaintext, public_key)
 	res = requests.post(
 		url=server_url(server_base_url, '/items'),
+		params=dict(batch=batch_id) if batch_id else None,
 		headers=dict(Authorization=f'Bearer {auth_token}'),
-		json=dict(target_type=target_type, target_id=target_id, item_encrypted_b64=item_encrypted_b64))
+		json=dict(
+				target_type=target_type, 
+				target_id=target_id, 
+				category=category, 
+				data_encrypted_b64=data_encrypted_b64, 
+				template=template_id))
 	if res.status_code == 200:
 		return 'item created'
 	else:
@@ -40,13 +54,17 @@ def add_item(server_base_url, auth_token, target_type, target_id, item_plaintext
 def add_items(
 		server_base_url, 
 		auth_token, 
-		items, 
-		content_field='content',
-		template_file=None,
 		target_type_field='target_type',
 		target_id_field='target_id',
+		category_field='category',
+		template_field='template',
+		items, 
+		content_field='content',
+		template_id_field='template',
 		override_target_type=None, 
 		override_target_id=None,
+		override_category=None,
+		override_template_id=None,
 		dry_run=False,
 		log_fn=print):
 	template = None
