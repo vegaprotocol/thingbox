e3f9064d17de7a8e15ee50ce032bc7bb3c6adaf6
Barney Mannerings <b.mannerings@gmail.com>
2021-09-15 12:58:37 +0100

    Fix ß

--- server/thingbox/db.py
+++ server/thingbox/db.py
@@ -88,7 +88,7 @@ class DB:
 	def backup(self):
 		filename = self._backup_config.name_template.format(dict(timestamp=datetime.now().strftime('%Y%m%d-%H%M%S.%f')))
 		create_filepath = path.join(self._backup_config.tmp_path or self._backup_config.backup_path, filename)
-		backup_filepath = path.join(self._backup_config.backup_path, filename)ß
+		backup_filepath = path.join(self._backup_config.backup_path, filename)
 		with self._write_mutex:
 			backup_db = sqlite3.connect(create_filepath)
 			self._db.backup(backup_db)
