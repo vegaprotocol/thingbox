b1bde9ff18981135954c9286ba508e9ae69b4897
Barney Mannerings <b.mannerings@gmail.com>
2021-10-08 00:49:18 +0100

    Usability: focus check user after returning results for quick fire

--- client/src/UserCheck.svelte
+++ client/src/UserCheck.svelte
@@ -3,6 +3,7 @@
 	
 	let checkUserId = ''
 	let checkResults = []
+	let	checkUserInput
 	
 	async function doCheck() {
 		checkResults = ['Querying items for: ' + checkUserId]
@@ -10,13 +11,15 @@
 		if (checkResults.length === 0) {
 			checkResults = ['No items for: ' + checkUserId]
 		}
+		checkUserInput.focus()
+		checkUserInput.select()
 	}
 </script>
 
 <section>
 	<h3>Item check</h3>
 	<p>Enter a user ID or screen name below to check what items are stored for that user.</p>
-	<p><input bind:value={checkUserId}>&nbsp;&nbsp;<button on:click={doCheck}>Check items</button></p>
+	<form><p><input bind:this={checkUserInput} bind:value={checkUserId}>&nbsp;&nbsp;<button type="submit" on:click|preventDefault={doCheck}>Check items</button></p></form>
 	{#each checkResults as lr}
 		<pre>{typeof lr === 'string' ? lr : JSON.stringify(lr)}</pre>
 	{/each}
