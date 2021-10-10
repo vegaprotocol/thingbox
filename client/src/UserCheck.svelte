b8e29ccca9171c3fc2b45b3519c955fa2fe96642
Barney Mannerings <b.mannerings@gmail.com>
2021-10-10 15:44:05 +0100

    Make lookup friendlier

--- client/src/UserCheck.svelte
+++ client/src/UserCheck.svelte
@@ -21,6 +21,13 @@
 	<p>Enter a user ID or screen name below to check what items are stored for that user.</p>
 	<form><p><input bind:this={checkUserInput} bind:value={checkUserId}>&nbsp;&nbsp;<button type="submit" on:click|preventDefault={doCheck}>Check items</button></p></form>
 	{#each checkResults as lr}
-		<pre>{typeof lr === 'string' ? lr : JSON.stringify(lr)}</pre>
+		{#if typeof lr === 'string' }
+			<pre>{lr}</pre>
+		{:else}
+			<details>
+				<summary>{lr.category} <span style="font-weight: normal;">(added {lr.created})</span></summary>
+				<pre>{JSON.stringify(lr, 2)}</pre>
+			</details>
+		{/if}
 	{/each}
 </section>
